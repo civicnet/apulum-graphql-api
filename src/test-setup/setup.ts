b9719f718c0b3123ae2cea429a8c6b9dd45aecd9
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-05-16 15:38:51 +0300

    fix(heroku) install ts-node as a dependency for now

--- src/test-setup/setup.ts
+++ src/test-setup/setup.ts
@@ -2,6 +2,12 @@ import { startServer } from '../startServer';
 
 export const setup = async () => {
     const app = await startServer();
-    const { port } = app.address();
+    const address = app.address();
+
+    let port = 0;
+    if (typeof address !== "string") {
+      port = address.port;
+    }
+
     process.env.TEST_HOST = `http://127.0.0.1:${port}`;
 };
