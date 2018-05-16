6cc0f9257133e06fc68c1a4c78284a2aad1367eb
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-05-16 14:15:30 +0300

    fix(login) no middleware

--- src/modules/me/middleware.ts
+++ src/modules/me/middleware.ts
@@ -1,14 +1,23 @@
 import { Resolver } from "../../types/graphql-utils";
 // import { logger } from "../../utils/logger";
 
-export default async (resolver: Resolver, ...params: any[]) => {
-  if (!params[2].session || !params[2].session.userId) {
+// export default async (resolver: Resolver, ...params: any[]) => {
+  /*if (!params[2].session || !params[2].session.userId) {
     return null;
-  }
+  } */
 
   // middleware
-  const result = await resolver(params[0], params[1], params[2], params[3]);
+  // const result = await resolver(params[0], params[1], params[2], params[3]);
   // afterware
 
-  return result;
-}
+  // return result;
+// }
+export default async (
+  resolver: Resolver,
+  parent: any,
+  args: any,
+  context: any,
+  info: any
+) => {
+  return resolver(parent, args, context, info);
+};
